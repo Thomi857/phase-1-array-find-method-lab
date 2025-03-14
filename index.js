3da414db73b736c551ddccd20f2c392404852ebb
CliffLinusthomi <linusthomi2@gmail.com>
2025-03-14 19:44:24 +0300

    iterating

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+//code your solution here
+const record = [
+    { year: 2015, result: "W"},
+    { year: 2014, result: "N/A"},
+    { year: 2013, result: "L"},
+    //...
+  ]
+
+
+
+  function superbowlWin(data) {
+    // Use find() to check for a win
+    const win = data.find(obj => obj.result === "W");
+    return win ? win.year : undefined;
+  }
+
